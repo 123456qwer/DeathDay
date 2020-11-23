f906e65c5a60f1767ba689d40283face8335d9e5
123456qwer <346607313@qq.com>
2020-11-23 09:09:52 +0800

    first

---
+++ DeathDay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DeathDay
+//
+//  Created by Mac on 2020/11/23.
+//  Copyright © 2020 Macdddd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DeathDay/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  DeathDay
+//
+//  Created by Mac on 2020/11/23.
+//  Copyright © 2020 Macdddd. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
